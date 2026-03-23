799a4c1e0ac89582dc85a80bc0a0348714fa08b5
Linh <nguyentienhoanglinh@gmail.com>
2026-03-23 16:10:00 +0700

    add terraform vpc and subnet setup

---
+++ terraform/envs/dev/variables.tf
@@ -0,0 +1,43 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "aws-eks-devops-platform"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  type    = string
+  default = "10.0.11.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  type    = string
+  default = "10.0.12.0/24"
+}
